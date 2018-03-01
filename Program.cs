011b12c47dc0a79d847377a004d38f40d99162ec
saf <igrsaf@yahoo.com>
2018-03-01 23:32:39 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var config = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
+          var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseStartup<Startup>()
+            .UseConfiguration(config)
+            .Build();
+          host.Run();
+
+        }
+    }
+
+  internal class Startup
+  {
+    public Startup(IHostingEnvironment env)
+    {
+    }
+    public void Configure(IApplicationBuilder app,
+      IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync("Hello, world!");
+      });
+    }
+
+  }
+}
